5b536371a9ebe264700d270951df42afdf57f437
victor <vjose1903@outlook.es>
2020-03-18 09:35:56 -0400

    intento 2 de arreglo servidor

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module ServerRa
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.secret_key_base = "ad106d808db07d328b40eeedaf9bc8a97ed666b9d4ab443d6c31171717fa1834608c51802bd2c1024c60e29c0a29ef2c735511dbf0c0b9f555d8bdeb2c337a8e"
     config.middleware.use ActionDispatch::Flash
     config.active_record.default_timezone = :local
     config.api_only = true
