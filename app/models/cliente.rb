678828ca91a0bbc6f9e7cb834e053a0fe0011a9f
victor <vjose1903@outlook.es>
2020-08-05 08:56:45 -0400

    hacer cambios en el filtro de clientes

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -11,9 +11,35 @@ class Cliente < ApplicationRecord
 
   # =========================================================================================================================================================
 
-  #
-  #
-  # ;
+  def self.parsearClientesFiltro(clientes)
+    clientes.each do |cli|
+      if cli["doc_id"]
+        cli["documento_de_identidad"] = { id: cli["doc_id"], descripcion: cli["doc_descripcion"], documento: cli["doc_documento"], user_id: cli["id"] }
+      else
+        cli["documento_de_identidad"] = {}
+      end
+
+      cli.delete("doc_descripcion")
+      cli.delete("doc_id")
+      cli.delete("doc_documento")
+    end
+    return clientes
+  end
+
+  # =========================================================================================================================================================
+
+  def self.filtrarCliente(arg)
+    arg = arg === " " ? "" : arg
+    select_ = "SELECT c.*, doc.documento as doc_documento, doc.descripcion as doc_descripcion, doc.id as doc_id "
+    from_ = "FROM clientes c"
+    joins_ = "inner join documentos_de_identidad doc on c.id = doc.cliente_id "
+    where_ = "where lower(c.nombre || ' ' || c.apellido || ' ' || coalesce(doc.documento, '') ) like lower('%#{arg}%') AND estado = true"
+
+    query = "#{select_} #{from_} #{joins_} #{where_}"
+
+    my_query(query)
+  end
+  # =========================================================================================================================================================
 
   def self.CalculateBalanceCLiente(id, totalFactura, operacion)
     cliente = Cliente.find_by_id(id)
