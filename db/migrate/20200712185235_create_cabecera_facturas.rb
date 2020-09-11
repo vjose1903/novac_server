491f0cf0d7dab27f9c52b8c826ac09fcea9d68e5
victor <vjose1903@outlook.es>
2020-09-11 15:53:59 -0400

    agregue campos a cabecera facturas

--- db/migrate/20200712185235_create_cabecera_facturas.rb
+++ db/migrate/20200712185235_create_cabecera_facturas.rb
@@ -3,15 +3,25 @@ class CreateCabeceraFacturas < ActiveRecord::Migration[5.2]
     create_table :cabecera_facturas do |t|
       t.references :user, foreign_key: true
       t.references :cliente, foreign_key: true
-      t.string :forma_pago
+      t.references :tipo_factura, foreign_key: true
       t.integer :numero_factura
+      t.float :bruto
+      t.float :itbis
+      t.float :descuento
       t.float :total_factura
-      t.boolean :pagada
-      t.float :balance
-      t.boolean :tiene_nota
       t.float :devuelta
+      t.float :balance
+      t.boolean :pagada
+      t.boolean :tiene_nota
+      t.boolean :estado
+      t.boolean :is_nota
+      t.date :fecha_vencimiento
+      t.string :forma_pago
+      t.string :condicion
       t.string :noCliente_nombre
       t.string :noCliente_direccion
+      t.string :noCliente_telefono
+      t.string :numero_comprobante
 
       t.timestamps
     end
