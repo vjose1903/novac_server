5ea4b8d72d3a636427cf9f9acc1db2def74ee8f2
victor <vjose1903@outlook.es>
2020-10-02 00:48:33 -0400

    cree funcion en el modelo de detalle de recibos para crear el detalle de recibo

--- app/controllers/recibos_ingresos_controller.rb
+++ app/controllers/recibos_ingresos_controller.rb
@@ -28,36 +28,17 @@ class RecibosIngresosController < ApplicationController
         unless SecuenciaIngreso.last.update({ secuencia: @recibos_ingreso.numero_recibo })
           render json: SecuenciaIngreso.last.errors, status: :unprocessable_entity
         else
-          detalles = []
-          @recibos_ingreso.detalle_recibos.each_with_index do |d, idx|
-            detalle = DetalleRecibo.find_by_id(d["id"])
-
-            if detalle == [] || detalle == nil
-              detalle.cabecera_factura_id = d["cabecera_factura_id"]
-              detalle = DetalleRecibo.new
-            end
-            calculo_cabecera = CabeceraFactura.calculateBalanceFactura(detalle["cabecera_factura_id"], detalle["deposito"], (idx + 1))
-
-            if calculo_cabecera[:error]
-              render json: { msg: calculo_cabecera[:msg] }, status: :unprocessable_entity
-              raise ActiveRecord::Rollback
-            end
-
-            detalle.balance_anterior_factura = calculo_cabecera[:balance_anterior]
-            detalle.balance_factura = calculo_cabecera[:balance]
-            detalle.pago_total = detalle["pago_total"]
-            detalle.deposito = detalle["deposito"]
-            detalle.descripcion = detalle["descripcion"]
-            detalle.pago_a_tiempo = detalle["pago_a_tiempo"]
-
-            detalles.push detalle
-          end
-
+          detalles = DetalleRecibo.CreateDetalleRecibo(@recibos_ingreso)
           # puts detalles.to_json
           # render json: { msg: "pruebas" }, :status => :unprocessable_entity
           # raise ActiveRecord::Rollback
 
           @recibos_ingreso.detalle_recibos = detalles
+
+          unless @recibos_ingreso.save!
+            render json: @recibos_ingreso.errors, :status => :unprocessable_entity
+            raise ActiveRecord::Rollback
+          end
 
           id = @recibos_ingreso.cliente_id
           total = @recibos_ingreso.total
--- app/models/detalle_recibo.rb
+++ app/models/detalle_recibo.rb
@@ -1,4 +1,34 @@
 class DetalleRecibo < ApplicationRecord
   belongs_to :recibos_ingreso
   belongs_to :cabecera_factura
+
+  # ==========================================================================
+
+  def self.CreateDetalleRecibo(recibos_)
+    detalles = []
+    recibos_.detalle_recibos.each_with_index do |d, idx|
+      detalle = DetalleRecibo.find_by_id(d["id"])
+
+      if detalle == [] || detalle == nil
+        detalle.cabecera_factura_id = d["cabecera_factura_id"]
+        detalle = DetalleRecibo.new
+      end
+      calculo_cabecera = CabeceraFactura.calculateBalanceFactura(detalle["cabecera_factura_id"], detalle["deposito"], (idx + 1))
+
+      if calculo_cabecera[:error]
+        render json: { msg: calculo_cabecera[:msg] }, status: :unprocessable_entity
+        raise ActiveRecord::Rollback
+      end
+
+      detalle.balance_anterior_factura = calculo_cabecera[:balance_anterior]
+      detalle.balance_factura = calculo_cabecera[:balance]
+      detalle.pago_total = detalle["pago_total"]
+      detalle.deposito = detalle["deposito"]
+      detalle.descripcion = detalle["descripcion"]
+      detalle.pago_a_tiempo = detalle["pago_a_tiempo"]
+
+      detalles.push detalle
+    end
+    return detalles
+  end
 end
