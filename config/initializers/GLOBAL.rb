3cdde16765e203f61f4a0aea49a7d49bdf299915
victor <vjose1903@outlook.es>
2020-10-23 11:34:47 -0400

    log 3

--- config/initializers/GLOBAL.rb
+++ config/initializers/GLOBAL.rb
@@ -48,7 +48,7 @@ end
 def my_print_log(*args)
   is_show = ENV.fetch("RAILS_SHOW_LOG") { false }
 
-  Rails.logger.info " ↳ is_show>> #{is_show} ".red + " => (#{caller_locations.first})"
+  Rails.logger.info " ↳ my_print_log ->> #{is_show} ".red + " => (#{caller_locations.first})"
   if ActiveRecord::Type::Boolean.new.cast(is_show)
     args.each do |arg|
       Rails.logger.info arg
