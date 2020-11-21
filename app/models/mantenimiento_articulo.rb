9360c956d66d823105ac493602600a3ba722ca93
victor <vjose1903@outlook.es>
2020-11-21 10:56:33 -0400

    mas logs

--- app/models/mantenimiento_articulo.rb
+++ app/models/mantenimiento_articulo.rb
@@ -32,12 +32,14 @@ class MantenimientoArticulo < ApplicationRecord
   def self.get_one_articulo_by_date(date, articulo_id)
     
     fechaConHora = date.to_s.split(":")[0] + ":" + date.to_s.split(":")[1]
-    my_print_log("fechaConHora ==> ".red + "#{fechaConHora}")
     
     historico = []
     articulo = Articulo.find_by_id(articulo_id)
-    my_print_log("articulo ==> ".yellow + "#{articulo.to_json}")
+    my_print_log("fechaConHora ==> ".red + "#{fechaConHora}")
+    my_print_log("fecha articulo update ==> ".blue + "#{parsearDateTimeUTC(articulo["updated_at"])}")
 
+    my_print_log("articulo ==> ".yellow + "#{articulo.to_json}")
+    
     if fechaConHora + ":59" >= parsearDateTimeUTC(articulo["updated_at"])      
       historico.push(Articulo.parseal(articulo))
       
