0ab7c7c3216f7ddf296d2f130fa83aad59da6364
victor <vjose1903@outlook.es>
2020-12-24 10:32:19 -0400

    cambio de longitud nombre en ant salgo

--- app/models/reporte.rb
+++ app/models/reporte.rb
@@ -100,7 +100,7 @@ class Reporte < ApplicationRecord
             end
 
             total_cuentas += att['total_factura']
-            client = buscar_cliente(att, 13)
+            client = buscar_cliente(att, 12)
 
             att['cliente_nombre'] = client['nombre']
             att['cliente_rnc'] = client['rnc']
