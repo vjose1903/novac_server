5a2acbd731efe764587500bc408052e0d18df313
victor <vjose1903@outlook.es>
2021-01-22 12:12:33 -0400

    inhabilitar el editar facturas por el momento

--- app/controllers/cabecera_facturas_controller.rb
+++ app/controllers/cabecera_facturas_controller.rb
@@ -30,6 +30,9 @@ class CabeceraFacturasController < ApplicationController
   
   def updateFacturaById
     CabeceraFactura.transaction do
+      return render json: {msg:'Esta función esta inhabilitada por reparaciones!!!'}, status: 400
+      raise ActiveRecord::Rollback
+
       id = params[:id]
       respuesta = CabeceraFactura.updateFactura(id, params, current_user)
       render json: respuesta, status: respuesta[:status]
