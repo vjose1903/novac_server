47f550f5aebe13475d4baed348ff299e2241419d
victor <vjose1903@outlook.es>
2021-01-25 14:59:13 -0400

    quitando cosas a ver si puedo acceder al servidor

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,7 +4,7 @@
 set :output, "log/cron.log"
 
 # set :environment, "development"
-set :environment, "production"
+# set :environment, "production"
 
 # every :day, at: ["09:00 AM", "12:00 AM", "03:00 PM", "05:30 PM", "09:00 PM" ] do
 #     puts "-------- EJECUTANDO TAREAS PARA EL BACKUP --------"
