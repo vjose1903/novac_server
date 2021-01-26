04074af258c036a1d42717ead416dadf8b17dbbb
victor <vjose1903@outlook.es>
2021-01-26 10:46:30 -0400

    quitando backup

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,16 +1,16 @@
 
 #correr este comando cuando se modifique este archivo => whenever --update-crontab
 
-set :output, "log/cron.log"
+# set :output, "log/cron.log"
 
 # set :environment, "development"
-set :environment, "production"
+# set :environment, "production"
 
 # every :day, at: ["09:00 AM", "12:00 AM", "03:00 PM", "03:24 PM", "05:30 PM", "09:00 PM" ] do
 #     puts "-------- EJECUTANDO TAREAS PARA EL BACKUP --------"
 #     rake 'server_db:backup'
 # end
-every 1.minutes do
-    puts "-------- EJECUTANDO TAREAS PARA EL BACKUP --------"
-    rake 'server_db:backup'
-end
+# every 1.minutes do
+#     puts "-------- EJECUTANDO TAREAS PARA EL BACKUP --------"
+#     rake 'server_db:backup'
+# end
