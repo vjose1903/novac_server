4ae0871bb7c88012948af4c92a956eafcff5ec41
victorvasquez <victorvasquez@orkapi.com>
2021-10-28 17:06:10 -0400

    arreglando otra vez lo del numero de factura

--- app/controllers/cabecera_facturas_controller.rb
+++ app/controllers/cabecera_facturas_controller.rb
@@ -162,7 +162,7 @@ class CabeceraFacturasController < ApplicationController
     CabeceraFactura.transaction do
       att = cabecera_factura_params
 
-      num_factura_valid = CabeceraFactura.where({numero_factura: @numero_factura, tipo: att["tipo"] }).blank?
+      num_factura_valid = CabeceraFactura.where({numero_factura: @numero_factura, tipo: att["tipo"], tipo_factura_id: att["tipo_factura_id"] }).blank?
 
       if num_factura_valid
         resultCliente     = { :error => false }
