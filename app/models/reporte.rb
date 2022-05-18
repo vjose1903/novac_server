301e4cec6f07c92f5379abcc31aff8943092a233
vjose1903 <vjose1903@outlook.es>
2022-05-18 15:53:03 -0400

    viendo log

--- app/models/reporte.rb
+++ app/models/reporte.rb
@@ -37,6 +37,7 @@ class Reporte < ApplicationRecord
         end
 
         documento       = DocumentoDeIdentidad.where({ principal: true, suplidor_id: supli["id"] })
+				my_print_log("documento ".red + "#{documento.to_json}")
         suplidor["rnc"] = !documento.empty? ? documento["documento"] : '----------'
         return suplidor
     end
