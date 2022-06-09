81495c8a0b2bbd4b108ae8148d045287c22094d7
vjose1903 <vjose1903@outlook.es>
2022-06-09 10:05:48 -0400

    mergue con base de datos de brendy en la cual hice muchas modificaciones de rendimiento

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,8 +7,8 @@ set :output, {:standard => 'log/cron.log', :error => 'log/error.log'}
 
 set :environment, ENV['RAILS_ENV']
 
-# every :day, at: ["09:00 AM", "12:00 PM", "03:00 PM", "05:30 PM", "09:00 PM" ] do
-every 1.hours do
+every :day, at: ["09:00 AM", "12:00 PM", "03:00 PM", "05:30 PM", "09:00 PM" ] do
+# every 1.hours do
     rake 'server_db:backup'
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -448,10 +448,10 @@ ActiveRecord::Schema.define(version: 2022_05_18_145843) do
   create_table "permisos", force: :cascade do |t|
     t.string "nombre"
     t.string "descripcion"
-    t.string "controlador"
-    t.boolean "mostrar_front"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "controlador"
+    t.boolean "mostrar_front"
   end
 
   create_table "permisos_acciones", force: :cascade do |t|
@@ -504,10 +504,9 @@ ActiveRecord::Schema.define(version: 2022_05_18_145843) do
     t.string "nombre"
     t.string "descripcion"
     t.string "ruta_defecto"
-    t.boolean "estado"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["nombre", "descripcion", "estado"], name: "index_roles_on_nombre_and_descripcion_and_estado", unique: true, where: "(estado = true)"
+    t.boolean "estado"
   end
 
   create_table "roles_permisos_acciones", force: :cascade do |t|
