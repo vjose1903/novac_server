253afb70cf25bbf86a9a6808589536c2028e1dad
vjose1903 <vjose1903@outlook.es>
2022-06-13 09:00:16 -0400

    haciendo que se pueda vender sin inventario

--- app/models/movimientos_inventario.rb
+++ app/models/movimientos_inventario.rb
@@ -5,37 +5,23 @@ class MovimientosInventario < ApplicationRecord
   # --------------------------------------------------------------------------------------------------------------------------------
 
   def self.movimientos_de_inventario(movimiento, operador, fecha_movimiento, accion, padre )
-    # puts "----- 1 -----".red
     res          = Response.new
 		MovimientosInventario.transaction do
 
 			articulo   = Articulo.find_by_id(movimiento["articulo_id"])
-			# puts "movimiento[articulo_id]".blue + " #{movimiento[:articulo_id]}"
-			# puts "movimiento[articulo_id]".yellow + " #{movimiento["articulo_id"]}"
-			# puts "articulo".red + " #{articulo.to_json}"
-
 			if articulo.nombre != 'Transporte'
-				# puts "----- 2 -----".red
-
-				# puts "articulo.existencia ".yellow + "#{articulo.existencia }"
-				# puts "operador ".green + "#{operador }"
-				# puts "movimiento[cantidad_en_unidades] ".magenta + "#{movimiento["cantidad_en_unidades"] }"
-
 				mov      = eval("#{articulo.existencia} #{operador} #{movimiento["cantidad_en_unidades"]}")
-				# puts "----- 3 -----".red
+
+				mov      = 0 if mov < 0 && movimiento['vende_sin_inventario'].presence && movimiento['vende_sin_inventario']
 
 				if operador == "-" # --------- SALIDA ---------
-					# puts "----- 4 -----".red
+
 					if mov < 0
-						# puts "----- 5 -----".red
 						res.add_msg("Cantidad introducida para el articulo << #{articulo.nombre.titleize} >> excede la cantidad disponible en inventario. ")
 						res.set_status(HTTP_STATUS_CODE[:conflict])
 						return res
 					end
 				end
-				# puts "----- 6 -----".red
-				# fecha_fact = fecha_movimiento.fecha_equivalente.strftime("%d/%m/%Y")
-
 
 				motivo = ""
 
@@ -45,7 +31,6 @@ class MovimientosInventario < ApplicationRecord
 				motivo = movimiento['motivo'] if accion == 'movimiento'
 
 				movimientos_inventario                          = MovimientosInventario.new
-				# puts "----- 7 -----".red
 
 				movimientos_inventario.user_id                  = get_current_user["id"]
 				movimientos_inventario.articulo_id              = movimiento["articulo_id"]
@@ -55,32 +40,19 @@ class MovimientosInventario < ApplicationRecord
 				movimientos_inventario.motivo                   = motivo
 				movimientos_inventario.medida                   = movimiento["medida"] || movimiento["unidad"]
 				movimientos_inventario.tipo_salida              = accion == 'movimiento' ? movimiento['tipo_salida'] : nil
-				# puts "----- 8 -----".red
 
 				unless movimientos_inventario.save!
-					# puts "----- 9 -----".red
 					res.add_msgs(movimientos_inventario.errors.to_a)
 					res.set_status(HTTP_STATUS_CODE[:conflict])
 					return res
 				end
 
-				# puts "----- 10 -----".red
 				articulo.existencia = mov
 
 				articulo.valid?
-				# puts "articulo ".green + "#{articulo.to_json}"
 				my_print_log("articulo.errors.to_a ".red + "#{articulo.errors.to_a}")
 
-				if articulo.save!
-					# puts "----- 11 -----".red
-
-					# puts "::::::::::::::::::::::::::::::::::::::::::"
-					# puts "::::                                  ::::"
-					# puts "::::         #{operador == "-" ? "SALIDA " : "ENTRADA"} EXITOSA           ::::"
-					# puts "::::                                  ::::"
-					# puts "::::::::::::::::::::::::::::::::::::::::::"
-				else
-					# puts "----- 12 -----".red
+				unless articulo.save!
 					res.add_msgs(articulo.errors.to_a)
 					res.set_status(HTTP_STATUS_CODE[:conflict])
 				end
@@ -88,7 +60,6 @@ class MovimientosInventario < ApplicationRecord
 			end
 		end
 
-    # puts "----- 13 -----".red
     return res
   end
 end
