b98e2d8731e01d6aa5ef46349f734e8276e0ebc8
vjose1903 <vjose1903@outlook.es>
2022-06-13 09:32:09 -0400

    viendo si ahora funciona la prefactura

--- app/models/cabecera_factura.rb
+++ app/models/cabecera_factura.rb
@@ -555,7 +555,7 @@ class CabeceraFactura < ApplicationRecord
       if res_validado.status_valid
 
         factura_de         = params['FACTURA_DE']
-        factura_nueva      = params
+        factura_nueva      = params.with_indifferent_access
 
         factura_original   = CabeceraFactura.find_by_id(params["id"])
 
