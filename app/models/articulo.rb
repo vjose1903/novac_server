f0e551efe36cc15bd324724745fe723ae6fff8c7
vjose1903 <vjose1903@outlook.es>
2023-04-18 09:28:58 -0400

    solucion de la peticion de get actual price tenia un error

--- app/models/articulo.rb
+++ app/models/articulo.rb
@@ -285,11 +285,11 @@ class Articulo < ApplicationRecord
 
   def self.get_actual_price_detalles(params, parametros_opcionales)
     res                = Response.new()
-    ids                = params[:ids].split('','').map(&:to_i)
+    ids                = params[:ids].split(',').map(&:to_i)
 
     articulos          = Articulo.where(id: ids).includes(Articulo.models_includes)
 
-    res.set_data(articulos, {**parametros_opcionales})
+    res.set_data( articulos, { **parametros_opcionales } )
 
     return res
   end
