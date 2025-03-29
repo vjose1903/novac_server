86bafda4e0d003ee9aca86613e7ad44f425d1985
Victor Vasquez <vjose1903@outlook.es>
2025-03-29 07:03:04 -0400

    Agregando el sh de actualizar el proyecto

---
+++ .update_novac.sh
@@ -0,0 +1,33 @@
+sudo chown -R $USER ./proyects
+sudo chown -R $USER ./proyects/novac-server
+
+cd proyects/novac-server
+
+echo ""
+echo ""
+
+git reset --h
+
+echo ""
+echo ""
+echo "\e[95m  ======================================================================================= MOVERME A LA RAMA DEL CLIENTE \e[0m"
+echo ""
+git switch ADM
+
+echo ""
+echo ""
+echo "\e[95m  ================================================================================================= DETENER EL PROYECTO \e[0m"
+echo ""
+node ./scripts/start.js -p -d
+
+echo ""
+echo ""
+echo "\e[95m  ============================================================================================ BAJAR LOS CAMBIOS DE GIT \e[0m"
+echo ""
+git pull
+
+echo ""
+echo ""
+echo "\e[95m  ===================================================================================== EJECUTAR NUEVAMENTE EL PROYECTO \e[0m"
+echo ""
+node ./scripts/start.js -w -p -c agrodemi -b -t -u
