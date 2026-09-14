7301d9517d6171b601d6e76ce44845a300851988
vjose1903 <vjose1903@outlook.es>
2026-09-14 17:36:00 -0400

    mejorando campos opcionales en el cuadre

--- app/models/cuadre_caja_movimiento.rb
+++ app/models/cuadre_caja_movimiento.rb
@@ -3,6 +3,14 @@ class CuadreCajaMovimiento < ApplicationRecord
 
   MOVEMENT_GROUPS = %w[other_payment_methods additional_transfers].freeze
   PAYMENT_METHODS = %w[card check deposit bank_transfer petty_cash_check other].freeze
+  DEFAULT_DESCRIPTIONS = {
+    'card' => 'Tarjeta',
+    'check' => 'Cheque',
+    'deposit' => 'Depósito',
+    'bank_transfer' => 'Transferencia bancaria',
+    'petty_cash_check' => 'Cheque de caja chica',
+    'other' => 'Otro movimiento'
+  }.freeze
 
   belongs_to :cuadre_caja
 
@@ -19,5 +27,6 @@ class CuadreCajaMovimiento < ApplicationRecord
     self.movement_group = movement_group.to_s
     self.payment_method = payment_method.to_s
     self.description = description.to_s.strip
+    self.description = DEFAULT_DESCRIPTIONS.fetch(payment_method, 'Movimiento') if description.blank?
   end
 end
